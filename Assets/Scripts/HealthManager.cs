b34dda958316cc732c74ba201e872d73dda8c23e
Alan Sartorio <alan42ga@hotmail.com>
2023-08-19 03:54:33 -0300

    Add death sound

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -6,6 +6,7 @@ public class Health : MonoBehaviour
 {
     public float health;
     public AudioClip damageAudioClip;
+    public AudioClip deathAudioClip;
     private AudioSource audioSource;
 
     public bool Dead
@@ -26,10 +27,14 @@ public class Health : MonoBehaviour
     public void ApplyDamage(float damage)
     {
         health -= damage;
-        audioSource.PlayOneShot(damageAudioClip, 0.7f);
         if (health <= 0)
         {
             health = 0;
+            audioSource.PlayOneShot(deathAudioClip, 0.7f);
+        }
+        else
+        {
+            audioSource.PlayOneShot(damageAudioClip, 0.7f);
         }
     }
 }
