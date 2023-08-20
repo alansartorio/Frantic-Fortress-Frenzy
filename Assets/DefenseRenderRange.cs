938f448f3297c3910e7e7ec6945d1c9bd0f9306f
Alan Sartorio <alan42ga@hotmail.com>
2023-08-20 05:29:42 -0300

    Add line renderer for showing turret range

---
+++ Assets/DefenseRenderRange.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(LineRenderer))]
+public class DefenseRenderRange : MonoBehaviour
+{
+
+    [Range(3, 256)]
+    public int numSegments = 128;
+
+    void Start()
+    {
+        DoRenderer();
+        GetComponent<LineRenderer>().enabled = true;
+    }
+
+
+    public void DoRenderer()
+    {
+        float radius = GetComponent<SphereCollider>().radius;
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = numSegments + 1;
+        lineRenderer.useWorldSpace = false;
+
+        float deltaTheta = (float)(2.0 * Mathf.PI) / numSegments;
+        float theta = 0f;
+
+        for (int i = 0; i < numSegments + 1; i++)
+        {
+            float x = radius * Mathf.Cos(theta);
+            float z = radius * Mathf.Sin(theta);
+            Vector3 pos = new Vector3(x, 0.1f, z);
+            lineRenderer.SetPosition(i, pos);
+            theta += deltaTheta;
+        }
+    }
+}
