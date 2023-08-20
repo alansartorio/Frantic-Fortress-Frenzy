04e5025e6abd76aa2eaafac5548a849c8a27c23b
Alan Sartorio <alan42ga@hotmail.com>
2023-08-19 22:12:01 -0300

    Use rigidbody for moving enemies

--- Assets/Scripts/EnemyChase.cs
+++ Assets/Scripts/EnemyChase.cs
@@ -6,17 +6,19 @@ public class EnemyChase : MonoBehaviour
 {
     public float speed = 2;
     private Transform targetTransform;
+    private Rigidbody targetRigidbody;
 
 
     void Start()
     {
         targetTransform = GetComponent<Enemy>().target.GetComponent<Transform>();
+        targetRigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         var delta = targetTransform.position - transform.position;
         var direction = delta.normalized;
-        transform.position += direction * speed * Time.deltaTime;
+        targetRigidbody.AddForce(direction * speed, ForceMode.Force);
     }
 }
