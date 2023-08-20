a55eb7e29a305b84017e00e26b2c2611b511d89b
Alan Sartorio <alan42ga@hotmail.com>
2023-08-19 22:30:56 -0300

    Add enemy rotation to face target

--- Assets/Scripts/EnemyChase.cs
+++ Assets/Scripts/EnemyChase.cs
@@ -17,8 +17,17 @@ public class EnemyChase : MonoBehaviour
 
     void Update()
     {
-        var delta = targetTransform.position - transform.position;
+        Vector3 delta3 = targetTransform.position - transform.position;
+        var delta = new Vector2(delta3.x, delta3.z);
+
         var direction = delta.normalized;
-        targetRigidbody.AddForce(direction * speed, ForceMode.Force);
+
+        var forward3 = transform.forward;
+        var forward = new Vector2(forward3.x, forward3.z);
+
+        var angle = Vector2.SignedAngle(forward, delta);
+
+        targetRigidbody.AddTorque(Vector3.up * angle * -0.01f);
+        targetRigidbody.AddForce(new Vector3(direction.x, 0, direction.y) * speed, ForceMode.Force);
     }
 }
