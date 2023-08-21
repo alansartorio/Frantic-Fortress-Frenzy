888b9ca40f220fa9f544b948e77d5e4eea684aa0
Alan Sartorio <alan42ga@hotmail.com>
2023-08-21 10:12:35 -0300

    Add tile components to detect ground interaction

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    void OnMouseEnter()
+    {
+        // Debug.Log(String.Format("{0}", transform.position));
+    }
+}
