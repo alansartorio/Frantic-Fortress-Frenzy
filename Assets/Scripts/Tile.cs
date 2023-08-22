6f26f9a508152efda4c9a120fb37d9eb40961741
Alan Sartorio <alan42ga@hotmail.com>
2023-08-22 20:13:36 -0300

    Add hover indicator to tiles

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -5,8 +5,21 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    GameObject indicator;
+    
+    private void Start()
+    {
+        indicator = transform.Find("Indicator").gameObject;
+    }
+
     void OnMouseEnter()
     {
+        indicator.gameObject.SetActive(true);
         // Debug.Log(String.Format("{0}", transform.position));
+    }
+
+    void OnMouseExit()
+    {
+        indicator.gameObject.SetActive(false);
     }
 }
