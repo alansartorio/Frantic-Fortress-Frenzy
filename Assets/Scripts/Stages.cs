8d040ea33cb17107ff2767e2fbd689c28c6c0026
Alan Sartorio <alan42ga@hotmail.com>
2024-01-29 20:14:00 -0300

    WIP: start working in enemy progression

---
+++ Assets/Scripts/Stages.cs
@@ -0,0 +1,41 @@
+using System;
+
+public struct EnemyProportions
+{
+    public float First { get; private set; }
+    public float Second { get; private set; }
+    public float Third { get; private set; }
+
+    public EnemyProportions(float first, float second, float third)
+    {
+        First = first;
+        Second = second;
+        Third = third;
+    }
+}
+
+public class Stages
+{
+    public EnemyProportions GetProportions(float t)
+    {
+        var weights = GetWeights(t);
+
+        var sum = weights.First + weights.Second + weights.Third;
+        return new EnemyProportions(weights.First / sum, weights.Second / sum, weights.Third / sum);
+    }
+
+    public EnemyProportions GetWeights(float t)
+    {
+        var stage = (float)Math.Truncate(t);
+        var fract = t - stage;
+
+        return stage switch
+        {
+            0 => new EnemyProportions(1, fract, 0),
+            1 => new EnemyProportions(1, 1, fract),
+            2 => new EnemyProportions(1 - (fract / 2f), 1, 1),
+            3 => new EnemyProportions(0.5f, 1 - (fract / 2f), 1),
+            _ => new EnemyProportions(0.5f, 0.5f, 1),
+        };
+    }
+}
